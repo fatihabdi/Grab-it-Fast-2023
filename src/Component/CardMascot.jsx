d6fd48ab46d758b06b7eaf3451ec994c47081345
fatihabdi <dotaawtf23@gmail.com>
2023-04-30 01:23:57 +0700

    mascot preview on click

--- src/Component/CardMascot.jsx
+++ src/Component/CardMascot.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 export default function CardMascot(props) {
   const [ishover, sethover] = useState(false);
+  let [isOpen, setIsOpen] = useState(false);
 
   function MouseOver() {
     sethover(true);
@@ -13,11 +15,38 @@ export default function CardMascot(props) {
     <>
       <div onMouseOver={MouseOver} onMouseOut={MouseOut}>
         {ishover ? (
-          <img src={props.imghover} className='' alt="" />
+          <img src={props.imghover} onClick={() => setIsOpen(true)} className="" alt="" />
         ) : (
-          <img src={props.img} className='max-[768px]:w-[220px]' alt="" />
+          <img src={props.img} className="max-[768px]:w-[220px]" alt="" />
         )}
+        <Transition
+          show={isOpen}
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+          as={Fragment}
+        >
+          <Dialog
+            open={isOpen}
+            onClose={() => setIsOpen(false)}
+            className="fixed inset-0 z-50 overflow-y-auto"
+          >
+            <div className="flex items-center justify-center min-h-screen">
+              <div
+                className="fixed inset-0 w-screen h-full bg-black opacity-30"
+                aria-hidden="true"
+              />
+              <Dialog.Overlay className="fixed inset-0" />
+              <div className="z-50">
+                <img src={props.imghover} className="min-[300px]:w-[380px] md:w-[463px] min-[1440px]:w-[550px] min-[1920px]:w-[697px]" alt="" />
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
       </div>
     </>
-  )
+  );
 }
--- src/Pages/Mascot.jsx
+++ src/Pages/Mascot.jsx
@@ -15,11 +15,11 @@ export default function Mascot() {
   return (
     <div className="overflow-auto lineup">
       <Navbar />
-      <div className="flex flex-col md:h-screen items-center min-[300px]:pt-[130px] md:pt-[150px] justify-center">
+      <div className="flex relative flex-col md:h-screen items-center min-[300px]:pt-[130px] md:pt-[150px] justify-center">
         {/* <h1 className='uppercase font-Normal lg:text-[64px] lg:font-outline-4 text-transparent bg-clip-text bg-gradient-to-b from-white to-[#FFC700] tracking-tighter'>introducing our mascot</h1> */}
-        <img src={judulmascot} className="hidden md:block" alt="" />
+        <img src={judulmascot} className="absolute hidden top-[120px] md:block" alt="" />
         <img src={judulmascotmobile} className="md:hidden" alt="" />
-        <div className="md:flex hidden mt-[60px]">
+        <div className="md:flex hidden mt-[150px]">
           <CardMascot imghover={kasep} img={kasep1} />
           <CardMascot imghover={ujang} img={ujang1} />
           <CardMascot imghover={geulis} img={geulis1} />
